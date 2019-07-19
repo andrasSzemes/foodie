a8562578a11b93aaa143cef2d32684abf452c073
AndrasSzemes <andras@szemes.eu>
2019-07-19 13:27:04 +0200

    Implement the /foods GET endpoint

--- src/main/java/andras/szemes/foodie/controller/FoodController.java
+++ src/main/java/andras/szemes/foodie/controller/FoodController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -18,6 +19,9 @@ public class FoodController {
 
     @GetMapping
     public List<Food> getAllFoods() {
-        return null;
+        try { return foodService.getAll(); }
+        catch (NullPointerException e) {
+            return new ArrayList<>();
+        }
     }
 }
--- src/main/java/andras/szemes/foodie/service/FoodService.java
+++ src/main/java/andras/szemes/foodie/service/FoodService.java
@@ -1,10 +1,20 @@
 package andras.szemes.foodie.service;
 
+import andras.szemes.foodie.model.Food;
+import andras.szemes.foodie.repository.FoodRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.io.File;
+import java.util.List;
 
 @Service
 public class FoodService {
 
+    @Autowired(required = false)
+    private FoodRepository foodRepository;
+
+    public List<Food> getAll() {
+        return foodRepository.findAll();
+    }
 }
